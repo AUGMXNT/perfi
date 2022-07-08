bcb340186e60183d2a438f73301f3ccb9ab35aef
peloriante <peloriante@protonmail.com>
2022-07-08 13:13:39 +0800

    pass ports via env vars so we can allow CORS on them too

--- app_main.py
+++ app_main.py
@@ -1,21 +1,17 @@
-import argparse
+import os
 from time import sleep
 
 from uvicorn import Config
 
 from perfi.api import Server
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--apiPort", default="5001")
-parser.add_argument("--frontendPort", default="5002")
-args = parser.parse_args()
-
+API_PORT = int(os.environ.get("API_PORT", 5000))
+FRONTEND_PORT = int(os.environ.get("FRONTEND_PORT", 5001))
 HOST = "127.0.0.1"
-API_PORT = int(args.apiPort)
+
 config = Config("perfi.api:app", host=HOST, port=API_PORT, log_level="info")
 server = Server(config=config)
 
-FRONTEND_PORT = int(args.frontendPort)
 frontend_config = Config(
     "perfi.api:frontend_app", host=HOST, port=FRONTEND_PORT, log_level="debug"
 )
